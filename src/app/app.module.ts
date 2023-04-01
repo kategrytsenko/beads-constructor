d235f21bf9aebd8483292171c8ff159a028bac6b
Kate <grytsenko.kate.ua@gmail.com>
2023-04-01 18:00:26 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaintBlockComponent } from './paint-block/paint-block.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaintBlockComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/paint-block/paint-block.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PaintBlockComponent } from './paint-block.component';
+
+describe('PaintBlockComponent', () => {
+  let component: PaintBlockComponent;
+  let fixture: ComponentFixture<PaintBlockComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PaintBlockComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PaintBlockComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
