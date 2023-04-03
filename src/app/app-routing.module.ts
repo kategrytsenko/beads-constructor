b3edc615103780b0b94acdfa470e5f1d7900a418
Kate <grytsenko.kate.ua@gmail.com>
2023-04-03 20:03:16 +0300

    added components for the new structure and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { ConstructorPageComponent } from './constructor-page/constructor-page.component';
+import { SavedDesignsPageComponent } from './saved-designs-page/saved-designs-page.component';
+import { LoginComponent } from './auth/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: WelcomePageComponent
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'constructor',
+    component: ConstructorPageComponent
+  },
+  {
+    path: 'designs',
+    component: SavedDesignsPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,14 @@ import { PaintBlockModule } from './paint-block/paint-block.module';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SignupModule } from './auth/signup/signup.module';
+import { LoginModule } from './auth/login/login.module';
+import { WelcomePageModule } from './welcome-page/welcome-page.module';
+import { BeadsConstructorModule } from './beads-constructor/beads-constructor.module';
+import { BeadsConstructorItemModule } from './beads-constructor-item/beads-constructor-item.module';
+import { PaintingPanelModule } from './painting-panel/painting-panel.module';
+import { SavedDesignsPageModule } from './saved-designs-page/saved-designs-page.module';
+import { ConstructorPageModule } from './constructor-page/constructor-page.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,6 +23,15 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     FormsModule,
     CommonModule,
     NgbModule,
+
+    SignupModule,
+    LoginModule,
+    WelcomePageModule,
+    BeadsConstructorItemModule,
+    BeadsConstructorModule,
+    PaintingPanelModule,
+    SavedDesignsPageModule,
+    ConstructorPageModule
   ],
   providers: [],
   bootstrap: [AppComponent],
