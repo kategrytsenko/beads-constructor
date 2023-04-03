678dcd9d99c2a9e396acf9476c0312ebc12d1027
Kate <grytsenko.kate.ua@gmail.com>
2023-04-04 00:37:29 +0300

    refactoring: moved code from constructor page to painting panel; added mat icons for buttons

--- src/app/painting-panel/painting-panel.component.ts
+++ src/app/painting-panel/painting-panel.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-painting-panel',
   templateUrl: './painting-panel.component.html',
   styleUrls: ['./painting-panel.component.scss'],
 })
-export class PaintingPanelComponent {}
+export class PaintingPanelComponent {
+  @Input() savedColors!: string[];
+  @Input() selectedColor!: string;
+
+  @Output() onSelectSavedColor = new EventEmitter<string>();
+  @Output() onColorDelete = new EventEmitter<string>();
+  @Output() onSaveColor = new EventEmitter<void>();
+  @Output() onSetWhite = new EventEmitter<void>();
+
+  @Output() colorChanged = new EventEmitter<string>();
+
+  onColorChange(newColor: string) {
+    this.selectedColor = newColor;
+    this.colorChanged.emit(newColor);
+  }
+}
--- src/app/painting-panel/painting-panel.module.ts
+++ src/app/painting-panel/painting-panel.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { PaintingPanelComponent } from './painting-panel.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [PaintingPanelComponent],
-  imports: [],
-  providers: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatIconModule,
+    MatButtonModule
+  ],
   exports: [PaintingPanelComponent],
 })
 export class PaintingPanelModule {}
